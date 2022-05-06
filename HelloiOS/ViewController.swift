2413f0de44f90dd8087e6fb67b2a8a75c3772b96
Shri Sharma <shris124@uw.edu>
2022-05-05 17:27:01 -0700

    Finished build

--- HelloiOS/ViewController.swift
+++ HelloiOS/ViewController.swift
@@ -8,10 +8,12 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var myLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        myLabel.text = "Hello World!"
     }
 
 
